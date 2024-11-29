6bfee336aab7838937e86e333dc16c152024b33b
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2024-11-29 01:49:43 -0500

    Updated layout.tsx for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+// import localFont from "next/font/local";
+// import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
@@ -35,7 +35,7 @@ import './globals.css'; // Import global styles if you have them
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Scott Glorioso',
   description: 'Singer, Songwriter, Sound Engineer, Producer',
 };
