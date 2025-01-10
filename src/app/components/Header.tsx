efdb57318cee1f1e8d6b86f9f7a7ed32568224b4
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-10 00:10:30 -0500

    Set header background color and added icon hover grow

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ import LogoTagline from './LogoTagline';
 
 const Header = () => {
   return (
-    <header className="bg-black-900 rounded-lg mx-auto max-w-screen-xl px-6 py-4 flex items-center justify-between mt-4">
+    <header className="bg-black rounded-lg mx-auto max-w-screen-xl px-6 py-4 flex items-center justify-between mt-4">
       {/* Logo and Tagline */}
       <div className="mt-4 cursor-pointer">
         <LogoTagline />
@@ -22,10 +22,10 @@ const Header = () => {
 
       {/* Icons */}
       <div className="flex items-center space-x-4">
-        <a href="https://music.apple.com/us/artist/scott-glorioso/1637244084" target="_blank" className="text-white hover:text-gray-300 text-2xl">
+        <a href="https://music.apple.com/us/artist/scott-glorioso/1637244084" target="_blank" className="text-white hover:text-gray-300 hover:scale-110 text-3xl duration-300 mr-1">
           <FaApple />
         </a>
-        <a href="https://open.spotify.com/artist/2r4EbUh8eNVrjHb4KAS5rp?si=yvBkF0F4SuymbAPO4VSClw" target="_blank" className="text-white hover:text-gray-300 text-2xl">
+        <a href="https://open.spotify.com/artist/2r4EbUh8eNVrjHb4KAS5rp?si=yvBkF0F4SuymbAPO4VSClw" target="_blank" className="text-white hover:text-gray-300 hover:scale-110 duration-300 text-3xl">
           <FaSpotify />
         </a>
       </div>
