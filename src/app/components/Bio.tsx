8141ec6e93e8daab5672e0fb41f06b57a0050fe6
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-10 00:11:24 -0500

    Incorporated StreamOn comp into Bio

--- src/app/components/Bio.tsx
+++ src/app/components/Bio.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Image from 'next/image';
 import Profile from '../../../public/images/profile.png';
 // import Link from 'next/link';
+import StreamOn from './StreamOn';
 
 const Bio = () => {
   return (
-    <section className="flex flex-col md:flex-row items-center bg-zinc-900 p-8 mt-14 rounded-xl shadow-lg max-w-screen-lg mx-auto">
+    <section className="flex flex-col md:flex-row items-center bg-neutral-900 outline outline-neutral-800 p-8 mt-14 rounded-xl shadow-lg max-w-screen-xl mx-auto">
         {/* Image Section */}
         <div className="w-full md:w-1/3 flex-shrink-0 mb-6 md:mb-0">
             <Image
@@ -18,17 +19,18 @@ const Bio = () => {
         </div>
 
         {/* Text Section */}
-        <div className="w-full md:w-2/3 md:pl-8">
+        <div className="w-full md:w-2/3 md:pl-8 flex flex-col justify-center">
             <h2 className="text-5xl font-bold text-red-500 mb-4">Scott Glorioso</h2>
-            <p className="text-gray-400 mb-4 leading-relaxed">
+            <p className="text-gray-400 mb-6 leading-relaxed">
                 Scott has been a music lover all of his life. He loves well produced, catchy rock and pop. Scott is an experienced songwriter, producer, recording engineer and artist in his own right. He has a back catalog of over 150 original pop/rock songs that he has produced and recorded in his studio. He also does recording and production work for several other area musicians and projects.
             </p>
-            <a
+            {/* <a
                 href="#"
                 className="text-zinc-200 bg-red-500 hover:bg-red-600 px-6 py-2 rounded-md text-lg font-medium transition duration-300 mt-5"
             >
                 Learn More
-            </a>
+            </a> */}
+            <StreamOn />
         </div>
     </section>
   );
