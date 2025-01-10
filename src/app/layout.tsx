117206a9614fa68d9664f14e4d254c875aba3020
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-10 00:11:37 -0500

    Set site-wide background to black

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-black antialiased flex flex-col min-h-screen`}
       >
         {/* Header */}
         <div className="flex-shrink-0">
