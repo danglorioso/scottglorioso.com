62d33a800bf0814a06277450659a73b041c7e45a
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-11 00:05:15 -0500

    Cleaning up song type file

--- src/app/types/song.ts
+++ src/app/types/song.ts
@@ -1,16 +1,23 @@
 import { IconType } from "react-icons";
 
+// Purpose: This file defines the overall interface for the Song object. Import
+// this file in any component to access the interface that stores all data 
+// relevant to a repsective song. 
+
+// Define the interface for each streaming service, its link, and icon
 export interface StreamingLink {
   name: string;
   url: string;
-  icon: IconType; // Keep this as IconType
+  icon: IconType; // Use icons from react-icon library (imported at top)
 }
 
+// Define the array of credits 
 export interface Credit {
   role: string;
   name: string;
 }
 
+// Define the main Song interface
 export interface Song {
   coverImage: string;
   title: string;
@@ -19,7 +26,7 @@ export interface Song {
   audioFile: string;
   releaseDate: string;
   copyright: string;
-  streamingLinks: StreamingLink[];
-  credits: Credit[];
+  streamingLinks: StreamingLink[]; // Defined above
+  credits: Credit[]; // Defined above
   lyrics: string;
 }
