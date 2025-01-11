718cb7a8a8f0008b5b4a311c55d804ee6fa14879
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-11 15:13:42 -0500

    Added store sandbox page

---
+++ src/app/store/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Page() {
+    return (
+        <div>
+        <h1 className="text-3xl justify-center font-semibold text-white text-cetner">Store coming soon.</h1>
+        </div>
+    );
+}
