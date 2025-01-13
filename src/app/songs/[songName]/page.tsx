bac50143078fa7056c23ff8a334639bc6349d2f9
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-01-13 04:38:33 -0500

    Adding all-songs page

---
+++ src/app/songs/[songName]/page.tsx
@@ -0,0 +1,39 @@
+import { Metadata } from "next";
+import SongPage from "../../components/SongPage";
+import { notFound } from "next/navigation";
+import allSongData from "../../data/allSongData";
+
+// Generate metadata dynamically for each song
+export async function generateMetadata(props: { params: Promise<{ songName: string }> }): Promise<Metadata> {
+  const params = await props.params;
+  const songName = await params.songName; // Ensure params are awaited
+  const song = allSongData[songName];
+
+  // Case for no song
+  if (!song) return { title: "Song Not Found" };
+
+  return {
+    title: `${song.title} - ${song.artist}`, // Ex: "I Am Great - Scott Glorioso"
+    description: `Listen to ${song.title} by ${song.artist}`,
+  };
+}
+
+// Generate static paths (URLs) for all songs
+export async function generateStaticParams() {
+  return Object.keys(allSongData).map((songName) => ({ songName }));
+}
+
+// Main component for the song page
+export default async function SongNamePage(props: { params: Promise<{ songName: string }> }) {
+  // Note: using "next-async-request-api" codemod to fix async prop function in Next.js 15
+  const params = await props.params;
+  const songName = await params.songName;
+  const song = allSongData[songName];
+
+  if (!song) {
+    notFound(); // Use Next.JS 404 page is no song found
+  }
+
+  // Render SongPage component with song's props
+  return <SongPage song={song} />;
+}
