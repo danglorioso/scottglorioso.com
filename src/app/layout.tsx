dd2e1d500d5962b5a6b0bdc96f58faf6bf756170
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-05-04 21:45:06 -0400

    Changing global font to Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/next";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-black antialiased flex flex-col min-h-screen`}
       >
